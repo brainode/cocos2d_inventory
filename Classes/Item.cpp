e5fcc6e70b77bf73dc325daadbcd8ab642c67aa2
Pavel Tananykhin <admin@brainode.com>
2016-10-03 21:31:45 +0300

    Init commit
    Generated app from cocos2d

---
+++ Classes/Item.cpp
@@ -0,0 +1,5 @@
+//
+// Created by rusbaron on 10/3/16.
+//
+
+#include "Item.h"
---
+++ Classes/Item.h
@@ -0,0 +1,14 @@
+//
+// Created by rusbaron on 10/3/16.
+//
+
+#ifndef MYGAME_ITEM_H
+#define MYGAME_ITEM_H
+
+
+class Item {
+
+};
+
+
+#endif //MYGAME_ITEM_H
